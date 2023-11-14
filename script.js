39970efd9a8c09cee89d21769a284cd922af4755
jayasankar J P <scicopath01@gmail.com>
2023-11-14 14:16:55 +0530

    Add search bar

--- script.js
+++ script.js
@@ -164,6 +164,24 @@ function addToCart(fname){
     console.log(cartList)
     
 }
+function search_flower() {
+    
+    location='#fruits' 
+    let input = document.querySelector('.searchElement').value 
+    input=input.toLowerCase(); 
+    console.log(input)
+    let x = document.getElementsByClassName('custome-cards'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="list-item";                  
+        } 
+    } 
+    document.querySelector('.searchElement').focus()
+} 
 function loadHomePage(){
    
     fetch_find_fruits(randomCards)
